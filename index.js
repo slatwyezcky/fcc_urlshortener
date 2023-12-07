fe9e824551006883743b94a25572091e15e030f3
Pavel Slatvitskii <pavel.slatvitskiy@gmail.com>
2023-12-07 18:54:52 +0600

    implement

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const dns = require('dns');
 const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
@@ -17,25 +18,29 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post('/api/shorturl', function (req, res) {
-  try {
-    const { url } = req.body;
-    const regexp =
-      /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g;
-    if (!regexp.test(url)) return res.json({ error: 'invalid url' });
-    const id = new Date().getTime();
-    data[id] = url;
-    console.log(data);
-    res.json({ original_url: url, short_url: id });
-  } catch (error) {
-    return res.json({ error: 'invalid url' });
-  }
+app.post('/api/shorturl/', (req, res, next) => {
+  let original_url = req.body.url;
+
+  dns.resolveAny(original_url, (err) => {
+    if (err) {
+      res.json({
+        error: 'invalid url',
+      });
+    } else {
+      let id = new Date().getTime();
+      data[id] = original_url;
+      res.json({
+        original_url,
+        short_url: id,
+      });
+    }
+  });
 });
 
 app.get('/api/shorturl/:url', function (req, res) {
   const { url } = req.params;
   if (data.hasOwnProperty(url)) return res.redirect(data[url]);
-  res.json({ error: 'invalid url' });
+  else res.json({ error: 'invalid url' });
 });
 
 app.listen(port, function () {
