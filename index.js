76353d468138437c6d716c08aecd04cec01baea2
Pavel Slatvitskii <pavel.slatvitskiy@gmail.com>
2023-12-07 18:38:50 +0600

    implement

--- index.js
+++ index.js
@@ -18,14 +18,18 @@ app.get('/', function (req, res) {
 });
 
 app.post('/api/shorturl', function (req, res) {
-  const { url } = req.body;
-  const regexp =
-    /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g;
-  if (!regexp.test(url)) return res.json({ error: 'invalid url' });
-  const id = new Date().getTime();
-  data[id] = url;
-  console.log(data);
-  res.json({ original_url: url, short_url: id });
+  try {
+    const { url } = req.body;
+    const regexp =
+      /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g;
+    if (!regexp.test(url)) return res.json({ error: 'invalid url' });
+    const id = new Date().getTime();
+    data[id] = url;
+    console.log(data);
+    res.json({ original_url: url, short_url: id });
+  } catch (error) {
+    return res.json({ error: 'invalid url' });
+  }
 });
 
 app.get('/api/shorturl/:url', function (req, res) {
