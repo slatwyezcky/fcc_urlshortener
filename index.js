4f38da250613edd7a64d066de73601e75f53869e
Pavel Slatvitskii <pavel.slatvitskiy@gmail.com>
2023-12-07 18:29:16 +0600

    implement

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 require('dotenv').config();
+const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// Basic Configuration
 const port = process.env.PORT || 3000;
 
+const data = {};
+
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function (req, res) {
+  const { url } = req.body;
+  const id = new Date().getTime();
+  data[id] = url;
+  console.log(data);
+  res.json({ original_url: url, short_url: id });
 });
 
-app.listen(port, function() {
+app.get('/api/shorturl/:url', function (req, res) {
+  const { url } = req.params;
+  if (data.hasOwnProperty(url)) return res.redirect(data[url]);
+  res.json({ error: 'invalid url' });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
